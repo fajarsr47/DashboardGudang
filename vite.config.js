df48b9492f45e339003bc9df197c0c5b26dbdc15
fajarsr47 <mfajarsetiawanfajar400@gmail.com>
2025-12-26 07:32:54 +0700

    Custom Domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DashboardGudang/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
